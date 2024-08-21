96d92da0641770be01b3ee22154b3e482ae64c99
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-08-21 09:42:50 +0100

    Update getanimelist.py

--- search/data/getanimelist.py
+++ search/data/getanimelist.py
@@ -1,30 +1,48 @@
-# Run this for updating the anime.json
-# change <end_num> for last pagination from gogoanime anime list page
+# run this to get the new anime.json
+# Google Collab is your best choice when running
+# running this code .
+# when you are done running this code in Google Collab create a 
+# new cell and execute this code 
+
+# from google.colab import files
+# files.download('anime.json')
+# the end num can be changed to whatever number 
+#  you want to use 
 
 import requests
 import json
-import time
 
-url_template = "https://animoasa.glitch.me/animelist/{}"
+url_template = "https://animetize-api-orcin.vercel.app/anime-list?page={}"
 start_num = 1
 end_num = 91
 animelist = []
 
 for num in range(start_num, end_num + 1):
     url = url_template.format(num)
-    print("Getting data for page ", num)
+    print("Getting data for page", num)
     response = requests.get(url)
     data = response.json()
-    animelist.extend(data)
 
-with open('anime.json', 'w') as f:
-    json.dump(animelist, f)
+    # Check if the 'results' key exists in the response data
+    if 'results' in data:
+        for item in data['results']:
+            # Map the new structure to the desired format
+            anime_entry = {
+                "name": item.get("title"),
+                "url": item.get("id")
+            }
+            animelist.append(anime_entry)
+    else:
+        print(f"No 'results' key in the response for page {num}")
 
-with open('anime.json', 'r') as f:
+filename = 'anime.json'
+with open(filename, 'w') as f:
+    json.dump(animelist, f, indent=2)  # Added indent for better readability
+
+with open(filename, 'r') as f:
     count = json.load(f)
 
 num_objects = len(count)
 
-print("Done! Saved as anime.json")
+print("Done! Saved as", filename)
 print("Total Anime:", num_objects)
-time.sleep(1000)
