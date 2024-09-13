d4c9bbb7434b98f9dcb0f5e3ca2783ce68764c79
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-09-13 16:31:37 +0100

    Create download.py

---
+++ search/data/download.py
@@ -0,0 +1,5 @@
+# if you ran the getanimelist.py on Google collab ran this code in a cell to download the anime.json file 
+from google.colab import files  
+
+# Download the file  
+files.download('anime.json')
